389a2908160d2b3e051d20cdf5d30e4b6ab1c24a
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-04-10 09:48:35 +0300

    Product model done

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,20 +1,68 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
+const Product = require('../models/product')
 //endpoints 
 router.get('/',(req, res, next)=>{
+ /*
     res.status(200).json({
         message:"Handling GET request to /products"
     })
+    */
+   Product
+   .find()
+   .exec()
+   .then((docs)=>{
+    console.log(docs)
+    if(docs.length >= 0){
+        res.status(200).json({
+            message:"Products",
+            body:docs
+        })
+    }else{
+        res.status(404).json({
+            message:"Product not found"
+        })
+    }
+
+   })
+   .catch((err =>{
+    res.status(404).json({
+        error:err
+    })
+   }))
 })
 router.post('/',(req, res, next)=>{
+    /*
     const product= {
         name:req.body.name,
         price: req.body.price
     }
-    res.status(200).json({
+       res.status(200).json({
         message:"Product created",
         createdProduct: product
+    })
+    */
+   const product = new Product({
+                _id: new mongoose.Types.ObjectId,
+                name: req.body.name,
+                price: req.body.price
+   })
+ product
+ .save()
+ .then(result =>{
+    console.log(result)
+    res.status(201).json({
+        message:"Product created",
+        createdProduct:{
+            product
+        }
+    })
+})
+.catch(err =>{
+        res.status(500).json({
+            error:err
+        })
     })
 })
 router.get('/:productId',(req, res, next)=>{
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 const productRoute = require('./api/routes/products')
 const orderRoute = require('./api/routes/orders')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 //Testing 
@@ -13,6 +16,13 @@ app.use((req, res, next) =>{
     })
 })
 */
+mongoose.connect('mongodb://localhost:27017/eShop',)
+.then(()=>{
+    console.log("Connected to the database")
+})
+.catch(()=>{
+    console.log("Connection failed")
+})
 
 app.use('/products', productRoute)
 app.use('/orders', orderRoute)
